11bc6a04830b7c392e84bf3468026f447c97eb9b
Dominik.Kotecki <dominikus1910@gmail.com>
2022-01-17 15:44:43 +0000

    add getbyids usecase

--- internal/core/usecase/catalog.go
+++ internal/core/usecase/catalog.go
@@ -20,3 +20,17 @@ func NewGetProductByIdUseCase(service services.CatalogService) *getProductByIdUs
 func (uc *getProductByIdUseCase) Execute(ctx context.Context, id model.ProductId) (*model.Product, error) {
 	return uc.service.GetProductById(ctx, id)
 }
+
+type getProductByIdsUseCase struct {
+	service services.CatalogService
+}
+
+func NewGetProductByIdsUseCase(service services.CatalogService) *getProductByIdsUseCase {
+	return &getProductByIdsUseCase{
+		service: service,
+	}
+}
+
+func (uc *getProductByIdsUseCase) Execute(ctx context.Context, ids []model.ProductId) ([]*model.Product, error) {
+	return uc.service.GetProductByIds(ctx, ids)
+}
