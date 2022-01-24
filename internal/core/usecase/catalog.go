593c8ebb0fd9141c4e65d3958b2cf3b167378cda
Dominik.Kotecki <dominikus1910@gmail.com>
2022-01-24 18:06:58 +0000

    small changes

--- internal/core/usecase/catalog.go
+++ internal/core/usecase/catalog.go
@@ -3,7 +3,6 @@ package usecase
 import (
 	"context"
 
-	"github.com/hashicorp/go-multierror"
 	"github.com/micro-eshop/catalog/internal/core/model"
 	"github.com/micro-eshop/catalog/internal/core/services"
 )
@@ -34,35 +33,4 @@ func NewGetProductByIdsUseCase(service services.CatalogService) *getProductByIds
 
 func (uc *getProductByIdsUseCase) Execute(ctx context.Context, ids []model.ProductId) ([]*model.Product, error) {
 	return uc.service.GetProductByIds(ctx, ids)
-}
-
-type importProductsUseCase struct {
-	service   services.CatalogImportService
-	source    services.ProductsSourceDataProvider
-	publisher services.ProductCreatedPublisher
-}
-
-func NewImportProductsUseCase(service services.CatalogImportService, source services.ProductsSourceDataProvider) *importProductsUseCase {
-	return &importProductsUseCase{
-		service: service,
-		source:  source,
-	}
-}
-
-func (uc *importProductsUseCase) Execute(ctx context.Context) error {
-	data, err := uc.source.Provide(ctx)
-	if err != nil {
-		return err
-	}
-	err = uc.service.Store(ctx, data)
-	if err != nil {
-		return err
-	}
-	for _, product := range data {
-		publishErr := uc.publisher.Publish(ctx, services.NewProductCreated(product))
-		if publishErr != nil {
-			err = multierror.Append(err, publishErr)
-		}
-	}
-	return err
 }
---
+++ internal/core/usecase/import.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/micro-eshop/catalog/internal/core/services"
+)
+
+type importProductsUseCase struct {
+	service   services.CatalogImportService
+	source    services.ProductsSourceDataProvider
+	publisher services.ProductCreatedPublisher
+}
+
+func NewImportProductsUseCase(service services.CatalogImportService, source services.ProductsSourceDataProvider) *importProductsUseCase {
+	return &importProductsUseCase{
+		service: service,
+		source:  source,
+	}
+}
+
+func (uc *importProductsUseCase) Execute(ctx context.Context) error {
+	data, err := uc.source.Provide(ctx)
+	if err != nil {
+		return err
+	}
+	err = uc.service.Store(ctx, data)
+	if err != nil {
+		return err
+	}
+	for _, product := range data {
+		publishErr := uc.publisher.Publish(ctx, services.NewProductCreated(product))
+		if publishErr != nil {
+			err = multierror.Append(err, publishErr)
+		}
+	}
+	return err
+}
